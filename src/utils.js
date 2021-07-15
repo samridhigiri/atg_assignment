65fff5ac4987858ffc6d998652270e2c8194c55b
samridhigiri <samridhigiri@outlook.com>
2021-07-15 19:42:06 +0200

    time format changed

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,10 @@
 export const baseURL = 'https://www.atg.se/services/racinginfo/v1/api';
 export const sortProductsByTime = (a, b) =>new Date(b.startTime) - new Date(a.startTime);
 export const formatDate = function (date){
-    const hours = new Date(date).getHours();
-    const minutes = new Date(date).getMinutes();
+    const d = new Date(date);
+    const hours = d.getHours();
+    const minutes = d.getMinutes();
     const time = (hours<10 ?'0'+hours:hours)+':'+(minutes<10 ?'0'+minutes:minutes);
-    return time;
+    
+    return `${d.getFullYear()} - ${d.getMonth()} - ${d.getDate()} : ${time}`;
 };
